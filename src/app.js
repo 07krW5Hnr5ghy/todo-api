90fbe1235a8350af939d9275a6e60ff0b99f76f2
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-14 22:30:15 -0500

    feature(main): database connection tested

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const User = require('./models/User');
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to the To-Do List API.');
+app.get('/', async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.status(200).json({
+            message:'Welcome to the To-Do List API!',
+            users,
+        });
+    }catch(error){
+        res.status(500).json({message:'Database connection failed',error});
+    }
+    
 });
 
 module.exports = app;
