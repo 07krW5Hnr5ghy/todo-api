8836c7fcbd778d47a2199dc7b210b1d032bc81b5
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-15 21:18:59 -0500

    feature(main): controller for login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -31,4 +31,29 @@ const register = async (req,res) => {
     }
 }
 
-module.exports = {register};
+const login = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message:'Invalid email or password'});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({message:'Invalid email or password'});
+        }
+
+        const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET,{
+            expiresIn:'1h'
+        });
+
+        res.status(200).json({message:'Login successful',token});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:'Server error',error});
+    }
+}
+
+module.exports = {register,login};
